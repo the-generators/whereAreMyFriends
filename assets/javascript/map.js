6aabe986cc0a252bc610908dac775d50a7d13c3f
marina lyulko <marinalyulko@gmail.com>
2017-09-06 14:53:27 -0700

    image on map canvas with click handler

--- assets/javascript/map.js
+++ assets/javascript/map.js
@@ -7,7 +7,58 @@ var map = new mapboxgl.Map({
     zoom: 9 // starting zoom
 });
 
-var popup = new mapboxgl.Popup({closeOnClick: false})
-    .setLngLat([-74.50, 40])
-    .setHTML('<h1>Hello World!</h1>')
-    .addTo(map);
+// var popup = new mapboxgl.Popup({closeOnClick: false})
+//     .setLngLat([-74.50, 40])
+//     .setHTML('<h1>Hello World!</h1>')
+//     .setHTML('<img src=assets/images/instagram.jpg>')
+//     .addTo(map);
+
+
+
+map.on('load', function(coordinates, profilePic) {
+
+    map.loadImage('https://scontent.cdninstagram.com/t51.2885-15/s640x640/sh0.08/e35/21373019_114988352523105_8096550142267621376_n.jpg', function(error, image) {
+        if (error) throw error;
+        map.addImage('profilePic', image);
+        image.className = "modal-trigger";
+     	
+        console.log(image);
+        map.addLayer({
+            "id": "points",
+            "type": "symbol",
+            "source": {
+                "type": "geojson",
+                "data": {
+                    "type": "FeatureCollection",
+                    "features": [{
+                        "type": "Feature",
+                        "geometry": {
+                            "type": "Point",
+                            "coordinates": [-74.50, 40]
+                        }
+                    }]
+                }
+            },
+            "layout": {
+                "icon-image": "profilePic",
+                "icon-size": 0.25
+            }
+        });
+    });
+    console.log('here');
+    map.on('click', 'points', function(e) {
+   	 $('#modal1').modal();
+   	 console.log("jere");
+
+    });
+      map.on('mouseenter', 'points', function () {
+        map.getCanvas().style.cursor = 'pointer';
+    });
+       map.on('mouseleave', 'points', function () {
+        map.getCanvas().style.cursor = '';
+    });
+});
+
+ $(document).ready(function(){
+     $('.modal').modal();
+  });
