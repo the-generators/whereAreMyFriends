55fd18f473009c564371cd05549a91af63dbd608
Live Sound Systems <ets@lives-mbp.lan>
2017-09-06 23:28:03 -0700

    deleted starter pins on map. pins need to be generated by click now

--- assets/javascript/map.js
+++ assets/javascript/map.js
@@ -6,53 +6,53 @@ $(document).ready(function(){
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoid2F0c2VyZmFjZSIsImEiOiJjajZ4MXFsOGYxaG14MzNycGlrZjc2aTV6In0.i1eqahI6l-ClT-EXrN-PcA';
 
-var geojson = {
-    "type": "FeatureCollection",
-    "features": [
-        {
-            "type": "Feature",
-            "properties": {
-                "message": "Foo",
-                "iconSize": [60, 60]
-            },
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    -66.324462890625,
-                    -16.024695711685304
-                ]
-            }
-        },
-        {
-            "type": "Feature",
-            "properties": {
-                "message": "Bar",
-                "iconSize": [50, 50]
-            },
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    -61.2158203125,
-                    -15.97189158092897
-                ]
-            }
-        },
-        {
-            "type": "Feature",
-            "properties": {
-                "message": "Baz",
-                "iconSize": [40, 40]
-            },
-            "geometry": {
-                "type": "Point",
-                "coordinates": [
-                    -63.29223632812499,
-                    -18.28151823530889
-                ]
-            }
-        }
-    ]
-};
+// var geojson = {
+//     "type": "FeatureCollection",
+//     "features": [
+//         {
+//             "type": "Feature",
+//             "properties": {
+//                 "message": "Foo",
+//                 "iconSize": [60, 60]
+//             },
+//             "geometry": {
+//                 "type": "Point",
+//                 "coordinates": [
+//                     -66.324462890625,
+//                     -16.024695711685304
+//                 ]
+//             }
+//         },
+//         {
+//             "type": "Feature",
+//             "properties": {
+//                 "message": "Bar",
+//                 "iconSize": [50, 50]
+//             },
+//             "geometry": {
+//                 "type": "Point",
+//                 "coordinates": [
+//                     -61.2158203125,
+//                     -15.97189158092897
+//                 ]
+//             }
+//         },
+//         {
+//             "type": "Feature",
+//             "properties": {
+//                 "message": "Baz",
+//                 "iconSize": [40, 40]
+//             },
+//             "geometry": {
+//                 "type": "Point",
+//                 "coordinates": [
+//                     -63.29223632812499,
+//                     -18.28151823530889
+//                 ]
+//             }
+//         }
+//     ]
+// };
 
 
 
